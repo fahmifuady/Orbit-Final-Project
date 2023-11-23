04ceafaa6a441a0d4211e0d79bb0758db5d8f795
fahmifuady <husni.fuad22@gmail.com>
2023-11-23 16:09:57 +0700

    feat(ui): add tailwind css integration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    fontFamily: {
+      inter: ["Inter"],
+    },
+  },
+  plugins: [],
+};
